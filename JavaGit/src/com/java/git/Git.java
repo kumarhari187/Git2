7a320843d9bff65ff9f837a4a3bfb358b8e18cc8
GitUser <git@user.com>
2016-03-15 14:20:12 +0530

    Initial Checkin.

---
+++ JavaGit/src/com/java/git/Git.java
@@ -0,0 +1,24 @@
+package com.java.git;
+
+public class Git {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	    Value data = new Value();
+		
+		displayGreeting();
+		data.display();
+	
+	}
+	
+	private static void displayGreeting(){
+		
+		System.out.println("This is Main.java");
+		
+		
+	}
+		
+		
+		
+	}
